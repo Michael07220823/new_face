e936f8d74be557802b34e07a4e998db1d39cead0
Michael07220823 <michael31703@gmail.com>
2021-10-28 14:19:11 +0800

    Add setup.py file and update package name.

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf8") as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name="new_face",
+    version="0.0.1",
+    author="Overcomer",
+    author_email="michael31703@gmail.com",
+    description="Face Recognition Tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Michael07220823/new_face.git",
+    keywords="Face Recognition",
+    install_requires=["opencv-python", "opencv-contrib-python", "tensorflow", "mtcnn", "sklearn", "imutils", "cmake", "dlib", "scikit-image", "new_tools", "pydot"],
+    license="MIT License",
+    packages=setuptools.find_packages(include=["new_face", "new_face.*"], exclude=["__pycache__"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS"],
+)
